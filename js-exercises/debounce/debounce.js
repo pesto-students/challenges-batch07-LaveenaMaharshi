656b78fc122c9723203cddba6bda189a32fc3186
LaveenaMaharshi <laavinaamahharshi@gmail.com>
2021-03-01 15:04:47 +0530

    solution-day-11

---
+++ js-exercises/debounce/debounce.js
@@ -0,0 +1,10 @@
+function debounce(func, delay) {
+  let timeoutID;
+  return ((...args) => {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => func(...args), delay);
+  });
+}
+export { debounce };
